3d028af51e2a004dbb17999f5b865af679b21273
Nanda Harpreet <nanda.harpreet@canada.ca>
2019-12-16 14:59:50 -0500

    making changes to auth e.p

--- oidc-controller/src/VCAuthn/IdentityServer/Endpoints/AuthorizationEndpoint/AuthorizeEndpoint.cs
+++ oidc-controller/src/VCAuthn/IdentityServer/Endpoints/AuthorizationEndpoint/AuthorizeEndpoint.cs
@@ -96,7 +96,7 @@ namespace VCAuthn.IdentityServer.Endpoints
                 return VCResponseHelpers.Error(IdentityConstants.InvalidRedirectUriError);
             }
 
-           /*
+           
             if (clientResult.Client.RedirectUris.Any() && !clientResult.Client.RedirectUris.Contains(redirectUrl))
             
             {
@@ -110,7 +110,7 @@ namespace VCAuthn.IdentityServer.Endpoints
               _logger.LogDebug("Processing Authorize request-ISED err 2");
                return VCResponseHelpers.Error(IdentityConstants.InvalidRedirectUriError);
             }
-            */
+            
 
             var responseType = values.Get(IdentityConstants.ResponseTypeUriParameterName);
             if (string.IsNullOrEmpty(responseType))
